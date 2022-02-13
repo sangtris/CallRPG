d56801fe9104c7cd6cb1d801c74652fe5b6f7384
Tristansang <tristansang001@gmail.com>
2022-02-13 14:43:59 +0200

    we finished the code for the RPG now we want to move to step number 2

--- PGstandoff1.py
+++ PGstandoff1.py
@@ -1,8 +1,16 @@
-import numbers
 import random
 import math 
 
-number=1+random.random()*10
-print(number)
-number=math.floor(number)
-print(number)
+def get_random_number(min, max):
+    number=min+random.random()*(max-min)
+    number=math.floor(number)
+    return number
+
+validCharacters = "0123456789qwertzuiopasdfghjklyxcvbnm"
+random_password = ""
+
+for x in range(0, 9):
+    number = get_random_number(0, len(validCharacters))
+    random_password = random_password + validCharacters[number]
+
+print(random_password)
